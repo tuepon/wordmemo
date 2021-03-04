b2c7bd6b682cfc1962fc6d9b01c30e7222ee7d9f
Tomonori Ueno <t.uepon@gmail.com>
2021-03-04 10:46:03 +0700

    Initial Commit

---
+++ wordmemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordmemo
+//
+//  Created by SEZAX development on 04/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
